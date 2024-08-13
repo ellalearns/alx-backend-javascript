a89ebb3b405491ba8e9a3455874cae83e2a91599
ellalearns <ellalearnscopy@gmail.com>
2024-08-13 17:29:32 +0100

    task 100 es6 data manipulation

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,13 @@
+export const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+  const oldCount = weakMap.get(endpoint);
+
+  if (oldCount === undefined) {
+    weakMap.set(endpoint, 1);
+  } else if (oldCount >= 5) {
+    throw new Error('Endpoint load is high');
+  } else {
+    weakMap.set(endpoint, oldCount + 1);
+  }
+}
