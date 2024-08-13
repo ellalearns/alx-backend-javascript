ff4bad0f883747981582d54b1f30da219b9251e7
ellalearns <ellalearnscopy@gmail.com>
2024-08-13 16:28:28 +0100

    task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -8,9 +8,9 @@ export default function cleanSet(set, startString) {
   }
 
   const strLength = startString.length;
-  if (strLength === 0) {
-    return '';
-  }
+//   if (strLength === 0) {
+//     return '';
+//   }
 
   const answerString = [...set]
     .filter((value) => value.startsWith(startString))
